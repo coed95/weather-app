00f1a37a04c9049447bd808271def60a93324f89
coed95 <codecoed95@gmail.com>
2024-06-25 15:41:46 +0200

    Add modules and events for the buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+import { updateWeatherFields,
+         swapTemperatureScale } from "./dom.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchForm = document.getElementById("searchForm");
+    const searchBar = document.getElementById("searchBar");
+    const searchButton = document.getElementById("searchButton");
+    const scaleButton = document.getElementById("scaleButton");
+
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+    });
+
+    searchButton.addEventListener("click", () => {
+        if (searchBar.value === "") {
+            return;
+        }
+    
+        const dataWrapper = document.getElementById("dataWrapper");
+        dataWrapper.classList.remove("hidden");
+        updateWeatherFields(searchBar.value);
+    });
+
+    scaleButton.addEventListener("click", () => {
+        swapTemperatureScale();
+    });
+});
